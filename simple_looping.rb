76975cc844ca4eee0d8e4d3d04d6d2770738cc12
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-10 04:32:40 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,25 +16,6 @@ def times_iterator(number_of_times)
   end
 end
 
-
-def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
-  number_of_times.times do 
-    puts phrase 
-  end
-end
-
-
-
-
-
-
-
-
-
-
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 1
@@ -43,6 +24,11 @@ def while_iterator(number_of_times)
     counter += 1
   end
 end
+
+
+
+
+
 
 def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
